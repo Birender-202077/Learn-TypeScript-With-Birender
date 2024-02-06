65dccb3938a5023a057571791eba388f6b09286a
unknown <bsbawlia@gmail.com>
2024-02-06 14:32:33 +0530

    interface in TSC

--- first.ts
+++ first.ts
@@ -6,3 +6,29 @@ var someArray: string[] = [] // in case if we want to push any type of variable 
 someArray.push(a);
 someArray.push(b as any);   // other method of using any is by casting
 console.log(someArray);
+
+//typescript also provide interface to define the type of function inputs
+interface Person {
+    firstName: string,
+    lastName: string,
+    job?: job, //we can also create optional props using ?
+    isVisitor?: boolean
+}
+
+//using the original type also, we can define the type of the variables 
+type job = 'Engineer' | 'Programmer';
+
+//functions can also have types
+function generateEmail(input: Person, force?: boolean): string | undefined {
+    if (input.isVisitor && !force) {
+        return undefined
+    }
+    return `${input.firstName}234${input.lastName}@gmail.com`
+}
+
+console.log(generateEmail({
+    firstName: 'birender',
+    lastName: 'singh',
+    job: 'Engineer',
+    isVisitor: true
+}, true))
